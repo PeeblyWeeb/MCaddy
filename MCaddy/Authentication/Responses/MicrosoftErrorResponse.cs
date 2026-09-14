1742a3800977fb8594009e0d92617c8958b28eec
peeblyweeb <peebly@salamithecat.com>
2026-09-14 01:28:11 -0400

    git fixes

--- MCaddy/Authentication/Responses/MicrosoftErrorResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace MCaddy.Authentication.Responses;
-
-public record MicrosoftErrorResponse
-{
-    public required string Error { get; init; }
-    public required string ErrorDescription { get; init; }
-    public required string CorrelationId { get; init; }
-}
