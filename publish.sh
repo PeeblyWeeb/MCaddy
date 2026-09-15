d3852480b6785443ab6ef17ec0b850afb7c69a3c
peeblyweeb <peebly@salamithecat.com>
2026-09-15 00:19:15 -0400

    publish script should not use self contained

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-dotnet publish -c Release -r linux-x64 --self-contained true
-dotnet publish -c Release -r win-x64 --self-contained true
+dotnet publish -c Release -r linux-x64
+dotnet publish -c Release -r win-x64
